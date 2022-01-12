9b8e5e95dfa05b6a88ea1e46ef14b7252674294d
9th-A <97576068+9th-A@users.noreply.github.com>
2022-01-12 19:30:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var deadline = new Date("Jan 26, 2022 06:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var t = deadline - now;
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((t % (1000 * 60)) / 1000);
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  if (t < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML =
+      "Goal Achieved! Happiest Reppublic Day";
+    document.getElementById("cycle").style.animationPlayState = "paused";
+    document.getElementById("cycle").src ="a.jpg";
+  }
+}, 1000);
